862cb1ced162f2e1c9d504fda8f5be2274181a20
Henry Aguinaga <aguinagahenry@gmail.com>
2017-02-20 12:34:03 +0100

    Dropdown menu completed

--- StackViews2/ViewController.swift
+++ StackViews2/ViewController.swift
@@ -9,17 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var emojiView: [UIView]! {
+    
+        didSet {
+            emojiView.forEach{
+                $0.isHidden = true
+            }
+        }
+    }
+    
+    @IBOutlet weak var emojiText: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+   
+    
+    @IBAction func pizzaButton(_ sender: Any) {
+        hideButtons()
+        
+        emojiText.text = "🍕"
     }
+    
+    @IBAction func heartButton(_ sender: Any) {
+        hideButtons()
+        
+        emojiText.text = "❤️"
+        
+    }
+    @IBAction func upButton(_ sender: Any) {
+        hideButtons()
+        
+        
+        emojiText.text = "👍"
+    }
+    
+    @IBAction func elephantButton(_ sender: Any) {
+        hideButtons()
+        
+        emojiText.text = "🐘"
+        
+    }
+    
+    @IBAction func worldButton(_ sender: Any) {
+        hideButtons()
+        
+        emojiText.text = "🌏"
+        
+    }
+    @IBAction func sunButton(_ sender: Any) {
+        hideButtons()
+        
+        emojiText.text = "🌝"
+        
+    }
+    
+    @IBAction func appleButton(_ sender: Any) {
+        hideButtons()
+        
+        emojiText.text = "🍏"
+        
+    }
+    
+    
+    
 
-
+    @IBAction func onSettingsButtonPressed(_ sender: Any) {
+        
+        hideButtons()
+        
+    }
+    
+    func hideButtons() {
+        UIView.animate(withDuration: 0.3) {
+            self.emojiView.forEach {
+                
+                $0.isHidden = !$0.isHidden
+            }
+        }
+    }
 }
 
